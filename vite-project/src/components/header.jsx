51042cf38a22d7d8dd2f2f4354c425bbde3a9e8c
forkangi3d <takaya20050509@gmail.com>
2024-12-15 16:55:11 +0900

    added an icon

--- vite-project/src/components/header.jsx
+++ vite-project/src/components/header.jsx
@@ -3,10 +3,7 @@ const Header = () => {
   return (
     <>
       <div className="header-wrapper">
-        <img
-          src="../../public/y-ab-looper.svg"
-          alt="Youtube AB Looper's LOGO"
-        />
+        <img src="src\assets\y-ab-looper.svg" alt="Youtube AB Looper's LOGO" />
         <h1 className="title">YOUTUBE AB LOOPER</h1>
       </div>
     </>
